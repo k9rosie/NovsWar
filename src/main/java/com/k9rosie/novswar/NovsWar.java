73b33ad4aa0a33c908a47d1fc49fcf82045a993c
k9rosie <k9rosie@gmail.com>
2016-01-05 14:13:48 -0600

    config cache is now being initialized

--- src/main/java/com/k9rosie/novswar/NovsWar.java
+++ src/main/java/com/k9rosie/novswar/NovsWar.java
@@ -1,16 +1,22 @@
 package com.k9rosie.novswar;
 
+import com.k9rosie.novswar.config.ConfigCache;
+
 public class NovsWar {
 	
 	private NovsWarPlugin plugin;
 	private static NovsWar instance;
 	
+	private ConfigCache configCache;
+	
 	public NovsWar(NovsWarPlugin plugin) {
 		this.plugin = plugin;
+		
+		configCache = new ConfigCache(this);
 	}
 	
 	public void initialize() {
-		
+		configCache.initialize();
 	}
 	
 	public static NovsWar getInstance() {
