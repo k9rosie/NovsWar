2f3e64a1689e8c249dc9dbc25806d5d9b8b6c424
k9rosie <k9rosie@gmail.com>
2016-05-11 07:01:49 -0500

    changed Gamemode to be an abstract class instead of an interface

--- src/main/java/com/k9rosie/novswar/gamemode/Gamemode.java
+++ src/main/java/com/k9rosie/novswar/gamemode/Gamemode.java
@@ -2,9 +2,9 @@ package com.k9rosie.novswar.gamemode;
 
 import com.k9rosie.novswar.game.Game;
 
-public interface Gamemode {
+public abstract class Gamemode {
 
-    public void initialize();
-    public void setGame(Game game);
-    public String getName();
+    public abstract void initialize();
+    public abstract void setGame(Game game);
+    public abstract String getName();
 }
