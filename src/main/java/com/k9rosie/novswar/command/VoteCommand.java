8bb1d85c4bd72b6bda72eb6985feb4ba5d885a6c
k9rosie <k9rosie@gmail.com>
2016-07-22 17:27:01 -0500

    resolved an issue with the getNovsPlayer() method being removed

--- src/main/java/com/k9rosie/novswar/command/VoteCommand.java
+++ src/main/java/com/k9rosie/novswar/command/VoteCommand.java
@@ -18,7 +18,7 @@ public class VoteCommand extends NovsCommand {
     }
 
     public void execute() {
-    	NovsPlayer player = getNovsWar().getPlayerManager().getNovsPlayer((Player) getSender());
+    	NovsPlayer player = getNovsWar().getPlayerManager().getPlayers().get((Player) getSender());
     	if(game.getGameState().equals(GameState.POST_GAME)) {
 	        if(player.hasVoted() == false) {
 	        	player.getBukkitPlayer().sendMessage("Cast your Vote");
