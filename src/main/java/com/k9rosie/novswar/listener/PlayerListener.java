ab937d85c41e2cb1665c7dd466f0f665f58f4da5
k9rosie <k9rosie@gmail.com>
2016-07-29 16:25:07 -0500

    added a check to cancel damagebyentity event if the damagetype is caused by player contact

--- src/main/java/com/k9rosie/novswar/listener/PlayerListener.java
+++ src/main/java/com/k9rosie/novswar/listener/PlayerListener.java
@@ -80,7 +80,6 @@ public class PlayerListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerDamageByPlayer(EntityDamageByEntityEvent event) {
-        System.out.println("EntityDamageByEntityEvent: " + event.getFinalDamage());
         Player victimBukkitPlayer;
         Player attackerBukkitPlayer = null;
         boolean arrowDeath = false;
@@ -168,7 +167,6 @@ public class PlayerListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerDamage(EntityDamageEvent event) {
-        System.out.println("EntityDamageEvent: " + event.getFinalDamage());
     	Game game = novswar.getGameHandler().getGame();
         if (event.getEntity() instanceof Player) {
             Player bukkitPlayer = (Player) event.getEntity();
