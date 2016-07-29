5a992c1a2eca02fd8f3db939d85faad75a0c7d45
k9rosie <k9rosie@gmail.com>
2016-07-29 17:29:04 -0500

    created objective regiontype

--- src/main/java/com/k9rosie/novswar/util/RegionType.java
+++ src/main/java/com/k9rosie/novswar/util/RegionType.java
@@ -3,13 +3,15 @@ package com.k9rosie.novswar.util;
 public enum RegionType {
     BATTLEFIELD,
     INTERMISSION_GATE,
-    DEATH_REGION;
+    DEATH_REGION,
+    OBJECTIVE;
 
     public static RegionType parseString(String regionType) {
         switch (regionType.toLowerCase()) {
             case "battlefield": return RegionType.BATTLEFIELD;
             case "intermission_gate": return RegionType.INTERMISSION_GATE;
             case "death_region": return RegionType.DEATH_REGION;
+            case "objective": return RegionType.OBJECTIVE;
         }
         return null;
     }
